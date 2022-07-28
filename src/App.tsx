92f80c70d7e638248ba78516c940c82c21b7cd2c
FelipeKonig <felipe.konig@compasso.com.br>
2022-07-28 02:42:45 -0300

    Initial routers added

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './components/Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary"></Button>
-      <Button variant="secondary"></Button>
-      <Button variant="success"></Button>
-      <Button variant="danger"></Button>
-      <Button />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
